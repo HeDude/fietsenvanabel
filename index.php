810ae56f34ec1457ee4929d4a1b35f12ad6d6048
Bijbrengen <slagter@bijbrengen.nl>
2020-02-26 16:14:13 +0100

    First version for online, no index for sections

--- index.php
+++ index.php
@@ -1,180 +1,151 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Fietsen van Abel</title>
-<!--
-Magazee Template
-http://www.templatemo.com/tm-514-magazee
+    <title>Fietsen van Abel</title>
+    <!--
+    Magazee Template
+    http://www.templatemo.com/tm-514-magazee
 -->
-  <!-- load CSS -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">    <!-- Google web font "Open Sans" -->
-  <link rel="stylesheet" href="css/bootstrap.min.css">                                        <!-- https://getbootstrap.com/ -->
-  <link rel="stylesheet" href="css/templatemo-style.css">                                     <!-- Templatemo style -->
-  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-  <link rel="icon" href="favicon.ico" type="image/x-icon">
+<!-- load CSS -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400">    <!-- Google web font "Open Sans" -->
+<link rel="stylesheet" href="css/bootstrap.min.css">                                        <!-- https://getbootstrap.com/ -->
+<link rel="stylesheet" href="css/templatemo-style.css">                                     <!-- Templatemo style -->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
 
-  <script>
-    var renderPage = true;
+<script>
+var renderPage = true;
 
-    if(navigator.userAgent.indexOf('MSIE')!==-1
-      || navigator.appVersion.indexOf('Trident/') > 0){
-        /* Microsoft Internet Explorer detected in. */
-        alert("Please view this in a modern browser such as Chrome or Microsoft Edge.");
-        renderPage = false;
-    }
-  </script>
+if(navigator.userAgent.indexOf('MSIE')!==-1
+|| navigator.appVersion.indexOf('Trident/') > 0){
+    /* Microsoft Internet Explorer detected in. */
+    alert("Please view this in a modern browser such as Chrome or Microsoft Edge.");
+    renderPage = false;
+}
+</script>
 
 </head>
 
 <body>
-  <!-- Loader -->
-  <div id="loader-wrapper">
-    <div id="loader"></div>
-    <div class="loader-section section-left"></div>
-    <div class="loader-section section-right"></div>
-  </div>
-
- <div class="container">
-
-  <!-- 1st section -->
-  <section class="row tm-section">
-   <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0">
-    <div class="tm-flex-center p-5 tm-bg-color-primary tm-section-min-h">
-      <h1 class="tm-text-color-white tm-site-name">Fietsen van Abel</h1>
+    <!-- Loader -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
     </div>
-  </div>
-  <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-    <div class="tm-flex-center p-5">
-      <q class="tm-quote tm-text-color-gray"><img class="logo" src="img/logo_fietsenvanabel.png"><br/>Hier vind je oude racers die met veel passie, kennis van zaken en vakmanschap aan een tweede leven kunnen beginnen. Het zijn ook zonder uitzondering fietsen die een persoonlijke band verdienen, al was het maar omdat er van elk exemplaar mondiaal geen tweede te vinden zal zijn.</q>
+
+    <div class="container">
+
+        <!-- 1st section -->
+        <section class="row tm-section">
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0">
+                <div class="tm-flex-center p-5 tm-bg-color-primary tm-section-min-h">
+                    <h1 class="tm-text-color-white tm-site-name">Fietsen van Abel</h1>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <div class="tm-flex-center p-5">
+                    <q class="tm-quote tm-text-color-gray"><img class="logo" alt="logo" src="img/logo_fietsenvanabel.png"><br/>Hier vind je oude racers die met veel passie, kennis van zaken en vakmanschap aan een tweede leven kunnen beginnen. Het zijn ook zonder uitzondering fietsen die een persoonlijke band verdienen, al was het maar omdat er van elk exemplaar mondiaal geen tweede te vinden zal zijn.</q>
+                </div>
+            </div>
+        </section>
+
+        <!-- 2nd section -->
+        <section class="row tm-section tm-col-md-reverse">
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <div class="tm-flex-center p-5">
+                    <div class="tm-md-flex-center">
+                        <p class="mb-4">Waar anders dan in Italië kan zo’n prachtige racefiets zijn oorsprong vinden? Een Gios professional uit 1984, een frame dat overigens voor het eerst ook in andere kleuren leverbaar was dan Gios-blauw. Columbus SL buizen, korte wielbasis, en dus heerlijk nerveus rijden. Remmen doet niemand echt graag, maar als het dan moet, dan bij voorkeur met Campagnolo Deltaremmen, al liet de werking van die dingen eigenlijk wat te wensen over. Maar o, wat mooi, en dan zwijg ik nog over de Super Record derailleurs, Het Cinellistuur met gepantografeerde Cinelli stuurpen. Een klassiek voorbeeld van geweldig rijden en lekker kijken.</p>
+                        <p style="text-align: center;">&#10225;</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0">
+                <div class="tm-flex-center p-5 tm-bg-color-primary">
+                    <div class="tm-max-w-400 tm-flex-center tm-flex-col">
+                        <img src="img/abel.jpeg" alt="Image" class="rounded-circle mb-4">
+                        <p class="tm-text-color-white small tm-font-thin mb-0">“Ik zie een racefiets niet alleen als een functioneel apparaat. Het is een tot in de kleinste details geëvolueerd ding dat ook nog heel mooi kan zijn. Eigenlijk bijna kunstzinnig, vooral de exemplaren met slanke stalen frames.”</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- 3rd Section -->
+        <section class="row tm-section">
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0 text-center">
+                <img src="img/image-01.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <div class="tm-flex-center p-5">
+                    <div class="tm-flex-center tm-flex-col">
+                        <p>Ik heb wel een zwak voor Giants, omdat dit Taiwanese merk vooral in de jaren negentig vernieuwende technologie aan de man bracht tegen alleszins schappelijke prijzen. Deze Giant is één van de eerste modellen op de Nederlandse markt, een Speeder. Een instapmodel, maar het ding is netjes gemaakt en rijdt heel leuk. De fiets is ontdaan van vrijwel alle onderdelen die toch al amper nog bruikbaar waren, en ik heb het minimaal willen houden. Eén voorblad, één voor(rem), 8 speed, en toch ook geschikt voor het Limburgse heuvelland, al is die ene rem dan wel wat gewaagd. Eenvoudig maar ook uniek, en heel betaalbaar.</p>
+                        <p style="text-align: center;">&#10225;</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- 4th section -->
+        <section class="row tm-section tm-col-md-reverse">
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <div class="tm-flex-center p-5">
+                    <div class="tm-md-flex-center">
+                        <p>Een Mercier Special Tour de France, een hele mond vol voor een fiets uit de tijd dat de Franse fabrikanten eigenlijk aan alle kanten werden voorbijgestreeft door de concurrentie, vooral die uit het Verre Oosten. Deze Mercier heeft een Vitus frame en rijdt heerlijk, maar de bouw is toch enigszins met de Franse slag gedaan. Dat geeft het ding ook weer charme. Omdat de framestickers sterk aan het vergaan waren, heb ik met behulp van vernis verder verval in elk geval vertraagd zonder de patina aan te tasten. Ik hou van kleurgebruik, getuige de oranje velgen, het marineblauwe stuurlint, en een Gipiemme zadel met borduursel. Een soort ratbike waar niemand een buil aan kan vallen.</p>
+                        <p style="text-align: center;">&#10225;</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0 text-center">
+                <img src="img/image-02.jpg" alt="Image" class="img-fluid">
+            </div>
+        </section>
+
+        <!-- 5th Section -->
+        <section class="row tm-section">
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0 text-center">
+                <img src="img/image-03.jpg" alt="Image" class="img-fluid">
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
+                <div class="tm-flex-center">
+                    <div class="tm-pricing-table">
+                        <div class="tm-bg-color-secondary tm-text-color-white text-center tm-font-thin tm-pricing-header">
+                            <p class="mb-0 tm-pricing-header-title">Interesse</p>
+                            <p class="mb-0 tm-pricing-header-subtitle">Heb je interesse in een geheel eigen fiets, op basis van een bestaand exemplaar, of wil je een paar dingen aan je eigen fiets laten doen? Neem contact op!</p>
+                        </div>
+                        <div class="tm-pricing-body">
+                            <p></p>
+                            <a href="mailto:abel@bijbrengen.nl" class="btn btn-secondary">Interesse</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-  </div>
-</section>
 
-<!-- 2nd section -->
-<section class="row tm-section tm-col-md-reverse">
- <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-  <div class="tm-flex-center p-5">
-    <div class="tm-md-flex-center">
-      <p class="mb-4">Waar anders dan in Italië kan zo’n prachtige racefiets zijn oorsprong vinden? Een Gios professional uit 1984, een frame dat overigens voor het eerst ook in andere kleuren leverbaar was dan Gios-blauw. Columbus SL buizen, korte wielbasis, en dus heerlijk nerveus rijden. Remmen doet niemand echt graag, maar als het dan moet, dan bij voorkeur met Campagnolo Deltaremmen, al liet de werking van die dingen eigenlijk wat te wensen over. Maar o, wat mooi, en dan zwijg ik nog over de Super Record derailleurs, Het Cinellistuur met gepantografeerde Cinelli stuurpen. Een klassiek voorbeeld van geweldig rijden en lekker kijken.</p>
-      <p style="text-align: center;">&#10225;</p>
+    <!-- Footer -->
+    <div class="row">
+        <div class="col-lg-12">
+            <p class="text-center small tm-copyright-text mb-0">Copyright &copy; <span class="tm-current-year">2020</span> Abel Borghuis | <a href="mailto:abel@bijbrengen.nl" target="blank">abel@bijbrengen.nl</a></p>
+        </div>
     </div>
-  </div>
-</div>
-<div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0">
-  <div class="tm-flex-center p-5 tm-bg-color-primary">
-    <div class="tm-max-w-400 tm-flex-center tm-flex-col">
-      <img src="img/abel.jpeg" alt="Image" class="rounded-circle mb-4">
-      <p class="tm-text-color-white small tm-font-thin mb-0">“Ik zie een racefiets niet alleen als een functioneel apparaat. Het is een tot in de kleinste details geëvolueerd ding dat ook nog heel mooi kan zijn. Eigenlijk bijna kunstzinnig, vooral de exemplaren met slanke stalen frames.”</p>
-    </div>
-  </div>
-</div>
-</section>
+    <!-- load JS -->
+    <script src="js/jquery-3.2.1.slim.min.js">
+    </script>         <!-- https://jquery.com/ -->
+    <script>
 
-<!-- 3rd Section -->
-<section class="row tm-section tm-mb-30">
-  <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 p-0 text-center">
-    <img src="img/image-01.jpg" alt="Image" class="img-fluid">
-  </div>
-  <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6">
-    <div class="tm-flex-center p-5">
-      <div class="tm-flex-center tm-flex-col">
-        <p>Ik heb wel een zwak voor Giants, omdat dit Taiwanese merk vooral in de jaren negentig vernieuwende technologie aan de man bracht tegen alleszins schappelijke prijzen. Deze Giant is één van de eerste modellen op de Nederlandse markt, een Speeder. Een instapmodel, maar het ding is netjes gemaakt en rijdt heel leuk. De fiets is ontdaan van vrijwel alle onderdelen die toch al amper nog bruikbaar waren, en ik heb het minimaal willen houden. Eén voorblad, één (voorrem), 8 speed, en toch ook geschikt voor het Limburgse heuvelland, al is die ene rem dan wel wat gewaagd. Eenvoudig maar ook uniek, en heel betaalbaar.</p>
-        <p style="text-align: center;">&#10225;</p>
-      </div>
-    </div>
-  </div>
-</section>
+    /* DOM is ready
+    ------------------------------------------------*/
+    $(function(){
 
-<!-- 4th Section -->
-<section class="row tm-section tm-mb-30">
- <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8">
-  <div class="tm-flex-center pl-5 pr-5 pt-5 pb-5">
-    <div class="tm-md-flex-center">
-     <p>Een Mercier Special Tour de France, een hele mond vol voor een fiets uit de tijd dat de Franse fabrikanten eigenlijk aan alle kanten werden voorbijgestreeft door de concurrentie, vooral die uit het Verre Oosten. Deze Mercier heeft een Vitus frame en rijdt heerlijk, maar de bouw is toch enigszins met de Franse slag gedaan. Dat geeft het ding ook weer charme. Omdat de framestickers sterk aan het vergaan waren, heb ik met behulp van vernis verder verval in elk geval vertraagd zonder de patina aan te tasten. Ik hou van kleurgebruik, getuige de oranje velgen, het marineblauwe stuurlint, en een Gipiemme zadel met borduursel. Een soort ratbike waar niemand een buil aan kan vallen.</p>
-     <p style="text-align: center;">&#10225;</p>
-   </div>
- </div>
-</div>
-<div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 text-xl-right text-md-center text-center mt-5 mt-lg-0 pr-lg-0">
- <img src="img/image-02.jpg" alt="Image" class="img-fluid">
-</div>
-</section>
+        if(renderPage) {
+            $('body').addClass('loaded');
+        }
 
-<!-- 5th Section -->
-<section class="row tm-section tm-mb-30">
-  <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 p-md-0 text-md-center text-center mb-4 mb-lg-0">
-   <img src="img/image-03.jpg" alt="Image" class="img-fluid">
- </div>
- <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 pl-lg-4 pr-xl-0 mb-4 mb-lg-0">
-  <div class="tm-flex-center">
-   <div class="tm-pricing-table">
-    <div class="tm-bg-color-secondary tm-text-color-white text-center tm-font-thin tm-pricing-header">
-      <p class="mb-0 tm-pricing-header-title">Huur</p>
-      <p class="mb-0 tm-pricing-header-subtitle">(€25 per maand)</p>
-    </div>
-    <div class="tm-pricing-body">
-      <ul class="tm-feature-list">
-        <li>Ut sit amet augue elit</li>
-        <li>Vivamus eget tortor in ante</li>
-        <li>Scelerisque gravida</li>
-        <li>Vestibulum auctor</li>
-        <li>Condimentum sem</li>
-        <li>Suscipit arcu accumsan</li>
-      </ul>
-      <a href="mailto:abel@bijbrengen.nl" class="btn btn-secondary">Huur</a>
-    </div>
-  </div>
-</div>
-</div>
-<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4 pl-xl-0 mb-4 mb-lg-0">
-  <div class="tm-flex-center">
-   <div class="tm-pricing-table">
-    <div class="tm-bg-color-secondary tm-text-color-white text-center tm-font-thin tm-pricing-header">
-      <p class="mb-0 tm-pricing-header-title">Koop</p>
-      <p class="mb-0 tm-pricing-header-subtitle">(in overleg)</p>
-    </div>
-    <div class="tm-pricing-body">
-      <ul class="tm-feature-list">
-        <li>Vel eleifend enim</li>
-        <li>Morbi blandit metus</li>
-        <li>Fusce porta lectus vel elit</li>
-        <li>Cras eu dolor lorem</li>
-        <li>Pharetra et sapien</li>
-        <li>Quisque auctor non magna</li>
-      </ul>
-      <a href="mailto:abel@bijbrengen.nl" class="btn btn-secondary">Koop</a>
-    </div>
-  </div>
-</div>
-</div>
-</section>
-
-<!-- Footer -->
-<div class="row">
-  <div class="col-lg-12">
-    <p class="text-center small tm-copyright-text mb-0">Copyright &copy; <span class="tm-current-year">2020</span> Abel Borghuis | <a href="mailto:abel@bijbrengen.nl" target="blank">abel@bijbrengen.nl</a></p>
-  </div>
-</div>
-</div>
-<!-- load JS -->
-<script src="js/jquery-3.2.1.slim.min.js"></script>         <!-- https://jquery.com/ -->
-<script>
-
-  /* DOM is ready
-  ------------------------------------------------*/
-  $(function(){
-
-    if(renderPage) {
-      $('body').addClass('loaded');
-    }
-
-    $('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright
-  });
-
-</script>
-
+        $('.tm-current-year').text(new Date().getFullYear());  // Update year in copyright
+    });
+    </script>
 </body>
 </html>
